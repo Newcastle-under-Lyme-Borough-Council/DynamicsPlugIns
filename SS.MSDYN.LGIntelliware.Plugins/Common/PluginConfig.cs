78df126ce1d8890e4d15b9b73494f445494bc371
NULBC\trelf <lee.fear@newcastle-staffs.gov.uk>
2026-04-29 15:56:26 +0100

    fixed issues

--- SS.MSDYN.LGIntelliware.Plugins/Common/PluginConfig.cs
+++ SS.MSDYN.LGIntelliware.Plugins/Common/PluginConfig.cs
@@ -12,5 +12,6 @@ namespace SS.MSDYN.LGIntelliware.Plugins.Common
         public string ContactColumnLogicalName { get; set; } = string.Empty;
         public string ReferenceNumberColumnLogicalName { get; set; } = string.Empty;
         public string ServiceConfigurationColumnLogicalName { get; set; } = string.Empty;
+        public string IncidentIdColumnLogicalName { get; set; } = string.Empty;
     }
 }
